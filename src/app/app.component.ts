2aa1d1f95c75b49e90c28b8ed989fa147fe59251
hey-now <rubinand22@gmail.com>
2023-02-13 11:07:53 -0800

    adding task adding and removal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'Angular Todo-List';
+  tasks: Task[] = [
+    new Task('Add A Task!')
+  ]
+
+  add(newTask: string) {
+    this.tasks.push(new Task(newTask));
+  }
+
+  remove(exisitingTask: Task) {
+    var userConfirm = confirm(`Are you sure you want to remove the following task? \n "${exisitingTask}"`)
+    if(userConfirm) {
+      this.tasks = this.tasks.filter(task => task != exisitingTask);
+    }
+  }
+
+  markAsDone(task: Task) {
+    task.isDone = true;
+  }
+}
+
+class Task {
+
+constructor(public title: string) {
+
+}
+
+public isDone = false;
+
 }
